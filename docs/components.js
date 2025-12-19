9a134192aecc0ae3bbd57b08d273885c8e867eb5
asantagata <asantagata225@gmail.com>
2025-12-19 04:07:13 -0500

    syntax highlighting

--- docs/components.js
+++ docs/components.js
@@ -28,8 +28,6 @@ const sidebar = {
             id: 'sidebar-index',
             children: [
                 { name: 'Fruit UI', url: '/' },
-                { name: 'Test 1', url: '/' },
-                { name: 'Some Stupid Shit, Such As Google Images', url: 'https://images.google.com/' },
             ].map(entry => ({
                 tag: 'a',
                 href: entry.url,
@@ -38,3 +36,123 @@ const sidebar = {
         }
     ]
 }
+
+const TokenTypes = {
+    NEWLINE: 'newline', 
+    WHITESPACE: 'whitespace', 
+    ALPHANUMERIC: 'alphanumeric', 
+    SYMBOLIC: 'symbolic', 
+    DECLARE_KEYWORD: 'declare', 
+    CTRLFLOW_KEYWORD: 'ctrlflow', 
+    NUMBER: 'number', 
+    FUNCTION: 'function', 
+    OBJECT: 'object', 
+    COMMENT: 'comment', 
+    STRING: 'string',
+    QUOTE: 'quote',
+    THIS: 'this'
+}
+
+function codePointType(codePoint) {
+    if (codePoint === 10)
+        return TokenTypes.NEWLINE;
+    if (codePoint === 32 || codePoint === 9)
+        return TokenTypes.WHITESPACE;
+    if (codePoint === 95 || (codePoint >= 65 && codePoint <= 90) || (codePoint >= 97 && codePoint <= 122) || (codePoint >= 48 && codePoint <= 57))
+        return TokenTypes.ALPHANUMERIC;
+    if (codePoint === 39 || codePoint === 34 || codePoint === 96)
+        return TokenTypes.QUOTE;
+    return TokenTypes.SYMBOLIC;
+}
+
+function tokenize(code) {
+    let tokens = [], curToken = {type: undefined};
+    for (const codePointContent of code) {
+        const codePoint = codePointContent.codePointAt(0);
+        const type = codePointType(codePoint);
+        if (curToken.type === type) {
+            curToken.text += codePointContent;
+        } else {
+            if (curToken.type)
+                tokens.push(curToken);
+            curToken = {text: codePointContent, type: type};
+        }
+    }
+    if (curToken.type)
+        tokens.push(curToken);
+    return tokens;
+}
+
+function SyntaxHighlighting(code) {
+    const declareKeywords = new Set(['const', 'function', 'let', 'new', 'document']);
+    const ctrlflowKeywords = new Set(['break', 'do', 'while', 'for', 'in', 'of', 'if', 'else', 'return', 'import', 'from']);
+    const tokens = tokenize(code);
+    console.log([...tokens]);
+    let currentType = null;
+    let insideStringBreak = false;
+    let lastOpeningQuote = null;
+    for (let i = 0; i < tokens.length; i++) {
+        let skipThisCurrentTypeAssignment = false;
+        const token = tokens[i];
+        if (currentType !== TokenTypes.STRING && token.type === TokenTypes.SYMBOLIC && token.text === '//')
+            currentType = TokenTypes.COMMENT;
+        else if (currentType === TokenTypes.COMMENT && token.type === TokenTypes.NEWLINE)
+            currentType = null;
+        else if (currentType !== TokenTypes.COMMENT && token.type === TokenTypes.QUOTE) {
+            if (currentType === TokenTypes.STRING && lastOpeningQuote === token.text) {
+                currentType = null;
+                lastOpeningQuote = null;
+            } else if (!lastOpeningQuote) {
+                currentType = TokenTypes.STRING;
+                lastOpeningQuote = token.text;
+            }
+        }
+        else if (currentType === TokenTypes.STRING && token.text.startsWith('${')) {
+            currentType = null;
+            insideStringBreak = true;
+        } else if (insideStringBreak && token.text.endsWith('}')) {
+            currentType = TokenTypes.STRING;
+            insideStringBreak = false;
+            skipThisCurrentTypeAssignment = true;
+        }
+
+        if (currentType && !skipThisCurrentTypeAssignment) 
+            token.type = currentType;
+        else if (token.type === TokenTypes.QUOTE)
+            token.type = TokenTypes.STRING;
+
+        if (token.type === TokenTypes.ALPHANUMERIC) {
+            if (Number.isFinite(+token.text)) {
+                token.type = TokenTypes.NUMBER;
+            } else if (i < tokens.length - 1 && tokens[i + 1].text.startsWith('(')) {
+                token.type = TokenTypes.FUNCTION;
+            } else if (token.text[0] === token.text[0].toUpperCase()) {
+                token.type = TokenTypes.OBJECT;
+            } else if (declareKeywords.has(token.text)) {
+                token.type = TokenTypes.DECLARE_KEYWORD;
+            } else if (ctrlflowKeywords.has(token.text)) {
+                token.type = TokenTypes.CTRLFLOW_KEYWORD;
+            } else if (token.text === 'this') {
+                token.type = TokenTypes.THIS;
+            }
+        }
+    }
+    
+    return {
+        tag: 'pre',
+        children: {
+            tag: 'code',
+            children: tokens.map(({text, type}) => ({tag: 'span', class: `token ${type}`, children: text}))
+        }
+    };
+}
+
+function Codeblock(snippet) {
+    return {
+        children: [
+            SyntaxHighlighting(snippet.code),
+            snippet.result
+        ]
+    }
+}
+
