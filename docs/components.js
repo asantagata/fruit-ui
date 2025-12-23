de88b706e0075bb2418cd1b4bb86e15ee1ed4393
asantagata <asantagata225@gmail.com>
2025-12-23 10:23:43 -0500

    use jsdelivr in docs components

--- docs/components.js
+++ docs/components.js
@@ -1,5 +1,4 @@
-import { Router, navigate, getPage } from "./router.js";
-// import { Router, navigate, getPage } from "../router/src/router.js"; // fix this
+import { Router, navigate, getPage } from "https://cdn.jsdelivr.net/npm/@fruit-ui/router@latest/src/router.js";
 import { examples } from "./examples.js";
 
 const ARTICLES = [
